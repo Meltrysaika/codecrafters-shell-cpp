9f3b540d7d3d234355cd89d8499322bd47e224a8
Meltrysaika <2065444599@qq.com>
2025-12-30 04:06:20 +0800

    type

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,15 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <unordered_set>
 
 int main()
 {
 	// Flush after every std::cout / std:cerr
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
+
+	const std::unordered_set<std::string> builtin_commands = {"echo", "exit", "type"};
 
 	while (true)
 	{
@@ -33,6 +36,21 @@ int main()
 				std::cout << tokens[i];
 			}
 			std::cout << std::endl;
+		}
+		else if (command == "type")
+		{
+			// 期望格式 type <command>
+			if (tokens.size() >= 2)
+			{
+				std::string q = tokens[1];
+				if (builtin_commands.count(q))
+					std::cout << q << " is a shell builtin" << std::endl;
+				else
+					std::cout << q << ": not found" << std::endl;
+			}
+			// 否则直接输出 not found
+			else
+				std::cout << ": not found" << std::endl;
 		}
 		else
 			std::cout << command << ": command not found" << std::endl;
