0431cc1490cc36c3282afbdd92c40fc07c3d4248
Meltrysaika <2065444599@qq.com>
2025-12-30 02:49:34 +0800

    stage 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <string>
 
-int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+int main()
+{
+	// Flush after every std::cout / std:cerr
+	std::cout << std::unitbuf;
+	std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  while (true) {
-    std::cout << "$ ";
-    std::string input;
-    std::cin >> input;
-    std::cout << input << ": command not found" << std::endl;
-  }
-  
+	// TODO: Uncomment the code below to pass the first stage
+	while (true)
+	{
+		std::cout << "$ ";
+		std::string input;
+		std::cin >> input;
+		if (input == "exit")
+		{
+			break;
+		}
+		std::cout << input << ": command not found" << std::endl;
+	}
 }
